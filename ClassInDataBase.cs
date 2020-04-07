c6df051cbbe149485f46a17e306d699e4f7ca9fa
Vadim1 <mustvad@gmail.com>
2020-04-07 13:28:16 +0500

    Добавьте файлы проекта.

---
+++ ClassInDataBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+
+namespace StateTrafficInspectorate
+{
+    class ClassInDataBase
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-QJJ19PD\SQLEXPRESS;Initial Catalog=StateTrafficInspectorate;Integrated Security=True"); //это строка подключения базы данных
+
+        public void openConnection()
+
+        {
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+
+                connection.Open();
+
+        }
+
+        public void closeConnection()
+
+        {
+
+            if (connection.State == System.Data.ConnectionState.Open)
+
+                connection.Close();
+
+        }
+
+        public SqlConnection GetConnection()
+
+        {
+
+            return connection;
+
+        }
+    }
+}
